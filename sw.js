4c378d2230b246ec6a262f6d228b61664b5c73ce
sphodchasit-afk <sphodchasit@gmail.com>
2026-03-13 14:30:30 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,29 +1,37 @@
-const CACHE = 'apphub-shell-v1';
-const SHELL = ['/', '/index.html'];
+const CACHE = 'amulet-nexus-v3';
+const ASSETS = [
+  '/amulet/',
+  '/amulet/index.html',
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
+
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   const url = new URL(e.request.url);
   if (url.origin !== location.origin) return;
   e.respondWith(
     caches.match(e.request).then(cached => {
-      const fresh = fetch(e.request).then(res => {
-        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+      const fetched = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
         return res;
       }).catch(() => cached);
-      return cached || fresh;
+      return cached || fetched;
     })
   );
 });
